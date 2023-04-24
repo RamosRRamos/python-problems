258909a4fa6d3e9fb767e4cc2c36e3b9f775351c
Wesley <wesley.rl@tuta.io>
2023-04-23 23:06:28 -0300

    add new solved

--- python_write_a_function/write_a_function.py
+++ python_write_a_function/write_a_function.py
@@ -3,6 +3,13 @@ class LeapYearChecker:
         self.year = year
 
     def is_leap(self):
+        """
+        This is a method called is_leap defined within a class.
+        It checks if the year attribute of the class instance is a
+        leap year or not by returning True if it is divisible by 4
+        and not divisible by 100 unless it is also divisible by 400.
+        :return:
+        """
         return self.year % 4 == 0 and (
                 self.year % 100 != 0 or self.year % 400 == 0)
 
